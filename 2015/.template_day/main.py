24ad52293def7df3ec40f70c9c9bca882238e575
james3126 <jk3126@hotmail.co.uk>
2023-12-03 18:49:56 +0000

    Try again. Creating new 2015 template file

---
+++ 2015/.template_day/main.py
@@ -0,0 +1,13 @@
+# Advent of Code - Year 2015 - Day 1
+with open('input.txt', 'r') as f:
+    puzzle_input = f.read()
+
+def part_1():
+    pass
+
+def part_2():
+    pass
+
+if __name__ == "__main__":
+    part_1()
+    part_2()
