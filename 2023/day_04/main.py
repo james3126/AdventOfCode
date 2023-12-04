7e1085ce609bb5dcd52b90a7f410453cdc19d9de
james3126 <jk3126@hotmail.co.uk>
2023-12-04 14:57:48 +0000

    Day 4 part 2 complete

--- 2023/day_04/main.py
+++ 2023/day_04/main.py
@@ -12,30 +12,65 @@ def part_1():
 
     for line in puzzle_input.split('\n'):
         line = line.split(':')[1]
-        #winning_numbers = line.split('|')[0].split(' ')
         winning_numbers = [x for x in line.split('|')[0].split(' ') if x != '']
         your_numbers = [x for x in line.split('|')[1].split(' ') if x != '']
-        print(winning_numbers,' | ',your_numbers)
+        # print(winning_numbers,' | ',your_numbers)
 
         card_wins = 0
-        win_num = []
         for number in your_numbers:
             #number = number.strip(' ')
 
             # print(number)
             if number in winning_numbers:
                 card_wins += 1
-                win_num.append(number)
-        print(card_wins,win_num)
+
         total_points += 2**(card_wins - 1) if card_wins > 0 else 0
 
-        #print(winning_numbers)
+        # print(winning_numbers)
     
     answer = total_points
     print("Part 1: ",answer)
 
 def part_2():
-    answer = ''
+    # 1st card - 1 match = 1 extra copy of card 2
+    # 1st card - n match = 1 extra copy of cards 2->n+1
+    # 3rd card - 5 match = 1 extra copy of cards 4->9
+    # x card - n match = 1 extra copy of cards x+1->x+n+1
+
+    # Hold the number of card copies for later. This will always start at 1, and increase from there.
+    card_copies = [1 for i in range(len(puzzle_input.split('\n')))]
+
+    total_cards = 0
+
+    # Loop through all cards
+    for game_index,line in enumerate(puzzle_input.split('\n')):
+        # print("\nGame: ",game_index+1,"Copies:",card_copies[game_index])
+        line = line.split(':')[1]
+        winning_numbers = [x for x in line.split('|')[0].split(' ') if x != '']
+        your_numbers = [x for x in line.split('|')[1].split(' ') if x != '']
+        # print(winning_numbers,' | ',your_numbers)
+
+        # Check for winning numbers
+        card_wins = 0
+        for number in your_numbers:
+
+            if number in winning_numbers:
+                card_wins += 1
+
+        # print("total wins: ",card_wins)
+
+        # Loop through the wins to add wins onto each of the next games
+        for i in range(card_wins):
+            # print(f"Adding {card_copies[game_index]} copies to game",game_index+i+1+1) # Extra +1 for readability
+            
+            # Discard any inputs beyond the end of the games
+            if i < len(puzzle_input.split('\n')):
+                # print(card_copies[game_index+i+1],'+',card_copies[game_index])
+                card_copies[game_index+i+1] += card_copies[game_index]
+                # print("=",card_copies[game_index+i+1])
+
+    answer = sum(card_copies)
+    # print(card_copies)
     print("Part 2: ",answer)
 
 if __name__ == "__main__":
