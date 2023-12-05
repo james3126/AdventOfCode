8bf4b9087f0c14deeff34b8a64983f310debca45
james3126 <jk3126@hotmail.co.uk>
2023-12-05 01:51:21 +0000

    Borderline minified 2023 day 4 at this point

--- 2023/day_04/main_cleaned.py
+++ 2023/day_04/main_cleaned.py
@@ -2,37 +2,28 @@
 with open('input.txt', 'r') as f:
     puzzle_input = f.read().split('\n')
 
-def calc_wins(card):
-    card = card.split(':')[1].split('|')
-    winning_numbers = [x for x in card[0].split()]
-    your_numbers    = [x for x in card[1].split()]
-
-    card_wins = 0
-    for number in your_numbers:
-        if number in winning_numbers:
-            card_wins += 1
-
-    return card_wins
+def calc_wins(card): 
+    winning_numbers,your_numbers = map(lambda n: n.split(),card.split(':')[1].split('|'))
+    
+    return sum(map(lambda number: 1 if number in winning_numbers else 0,your_numbers))
 
 def part_1():
     total_points = 0
 
     for line in puzzle_input:
         card_wins = calc_wins(line)
-        total_points += 2**(card_wins - 1) if card_wins > 0 else 0
+        if card_wins > 0: total_points += 2**(card_wins - 1)
 
     print("Part 1: ",total_points)
 
 def part_2():
-    card_copies = [1]*len(puzzle_input)
+    inputs = len(puzzle_input)
+    card_copies = [1]*inputs
     total_cards = 0
 
     for game_index,line in enumerate(puzzle_input):
-        card_wins = calc_wins(line)
-
-        for i in range(card_wins):
-            if i < len(puzzle_input):
-                card_copies[game_index+i+1] += card_copies[game_index]
+        for i in range(calc_wins(line)):
+            if i < inputs: card_copies[game_index+i+1] += card_copies[game_index]
 
     print("Part 2: ",sum(card_copies))
 
